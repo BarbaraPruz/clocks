25636d4da63359a9efc143b43e5c8c4582e522d7
barbara <barbara.prusiewicz@gmailcom>
2019-03-27 14:19:59 -1000

    Add some styling for digital clocks

--- src/components/clock.js
+++ src/components/clock.js
@@ -4,8 +4,9 @@ class Clock extends Component {
   render() {
       console.log("Clock for",this.props.timezone);
     return (
-      <div className="Clock">
-          <p>{this.props.timezone} {this.props.time}</p>
+      <div className="clock digital-clock">
+          <p>{this.props.time}</p>
+          <p class="timezone">{this.props.timezone}</p>          
       </div>
     );
   }
