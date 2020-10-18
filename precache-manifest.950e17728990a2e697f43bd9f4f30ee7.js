f3cb999498f7073390b2bd73a4035c361a757676
barbara prusiewicz <barbara.prusiewicz@gmail.com>
2020-10-17 19:27:59 -1000

    Updates

---
+++ precache-manifest.950e17728990a2e697f43bd9f4f30ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66863b88ef145a83458dd2f28eedd1bc",
+    "url": "/clocks/index.html"
+  },
+  {
+    "revision": "fa6b132f651610e0ca42",
+    "url": "/clocks/static/css/main.c262a444.chunk.css"
+  },
+  {
+    "revision": "41d0425d5b21c9612a08",
+    "url": "/clocks/static/js/2.c22c5894.chunk.js"
+  },
+  {
+    "revision": "607f4dba556022527177500adaa9aa68",
+    "url": "/clocks/static/js/2.c22c5894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6b132f651610e0ca42",
+    "url": "/clocks/static/js/main.9dc72b4a.chunk.js"
+  },
+  {
+    "revision": "5fbf7357bc00618db7b1",
+    "url": "/clocks/static/js/runtime-main.046b737e.js"
+  }
+]);
